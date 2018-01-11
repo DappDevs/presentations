baeff7be231580e8e31bd8513af0a85942da4df5
Bryant <fubuloubu@gmail.com>
2018-01-11 12:59:20 -0500

    Moved presentation loader to it's own file

---
+++ load-presentation.js
@@ -0,0 +1,33 @@
+// Place all of your files here
+sourceUrls = [
+    'intro.md',
+    'content/whats-a-dapp.md',
+    'content/why-ethereum.md',
+    'content/ethereum-history.md',
+    'content/ethereum-background.md',
+    'content/ethereum-ecosystem.md',
+    'content/ethereum-future.md',
+    'content/how-to-dappdev.md',
+    'questions.md'
+];
+
+var xmlhttp = new XMLHttpRequest();
+
+var source = document.getElementById("source")
+
+for (var i = 0; i < sourceUrls.length; i++) {
+    xmlhttp.open('GET', sourceUrls[i], false);
+    xmlhttp.send();
+
+    if (i > 0) source.innerHTML += "---\n"
+    source.innerHTML += "\n" + xmlhttp.responseText;
+};
+
+var slideshow = remark.create({
+    ratio: '16:9',
+    navigation: {
+        scroll: false,
+        touch: true,
+        click: false
+    },
+});
